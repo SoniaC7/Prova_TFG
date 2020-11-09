8fa5da2fb59470c42c5d9cee172fdeff687d4585
SoniaC7 <59957248+SoniaC7@users.noreply.github.com>
2020-11-09 18:59:40 +0100

    PRove TFG

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-var app = new Vue({
-	el: '#app',
-	data:{
-		product: 'Socks',
-		description: 'A pair of warm, fuzzy socks'
-	}
 
+const app = Vue.createApp({
+	data: function(){
+		return{
+			product: 'Socks'
+		}
+	}
 })
