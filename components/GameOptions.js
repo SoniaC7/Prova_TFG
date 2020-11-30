a192fdfb016ec4848827ef94cb8e5b19eb1256d3
SoniaC7 <59957248+SoniaC7@users.noreply.github.com>
2020-11-30 10:50:21 +0100

    instagram changed for social media

--- components/GameOptions.js
+++ components/GameOptions.js
@@ -1,13 +1,13 @@
 const game = Vue.createApp({
     data(){
         return {
-            options: ['Real vs Instagram', 'Fake photos']
+            options: ['Real vs Social Media', 'Fake photos']
         }
     },
     methods:{
         changePage(option){
 
-			if (option === 'Real vs Instagram') { window.location.href = "./instagram.html"; }
+			if (option === 'Real vs Social Media') { window.location.href = "./instagram.html"; }
 			if (option === 'Fake photos') { window.location.href = "./fake.html"; }
 			
 		}
