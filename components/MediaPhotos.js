c3a7d18955fbcacf6c7d86a6de29ba035b62c6f3
SoniaC7 <59957248+SoniaC7@users.noreply.github.com>
2020-12-21 10:02:35 +0100

    codi easy pulit

--- components/MediaPhotos.js
+++ components/MediaPhotos.js
@@ -13,21 +13,25 @@ const easy = Vue.createApp({
         increment_score(){
             this.score = this.score + 1;
         },
-        mark(el) {
-            el.style.border = "1px solid green";
-        },
         getImageMedia(index){
-            in_string = this.index.toString();
+            in_string = index.toString();
             this.fol_name = './assets/images/media/easy/'+ in_string + '/media.png';
             return this.fol_name;
         },
         getImageReal(index){
-            in_string = this.index.toString();
+            in_string = index.toString();
             this.fol_name = './assets/images/media/easy/'+ in_string + '/real.png';
             return this.fol_name;
         },
         next(){
             this.index = this.index + 1;
+            this.log(this.index);
+
+            m_name = this.getImageMedia(this.index);
+            document.getElementById('mediaId').src=m_name;
+
+            r_name = this.getImageReal(this.index);
+            document.getElementById('realId').src=r_name;
         }
     }
 })
