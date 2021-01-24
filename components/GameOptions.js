9e2ab47212ef918f58990c1a76f6c90e3729aad7
SoniaC7 <59957248+SoniaC7@users.noreply.github.com>
2021-01-24 23:14:42 +0100

    Progress bar Easy.html

--- components/GameOptions.js
+++ components/GameOptions.js
@@ -1,15 +1,33 @@
 const game = Vue.createApp({
     data(){
         return {
-            options: ['Real vs Social Media', 'Fake photos']
+            options: ['Real vs Social Media', 'Fake photos', 'Badges'],
+            badges: [
+                {
+                    id: 1,
+                    text: "Detective apprentice",
+                    example: "./assets/images/badges/apprentice.png",
+                    earned: false
+                },{
+                    id: 2,
+                    text: "Junior detective",
+                    example: "./assets/images/badges/apprentice_hat.png",
+                    earned: false
+                }
+             ]
         }
     },
     methods:{
         changePage(option){
 
 			if (option === 'Real vs Social Media') { window.location.href = "./instagram.html"; }
-			if (option === 'Fake photos') { window.location.href = "./fake.html"; }
+            if (option === 'Fake photos') { window.location.href = "./fake.html"; }
+            if (option === 'Badges') { window.location.href = "./badges.html"; }
 			
-		}
+        },
+        go_to_menu(){
+            window.location.href = "./game.html";
+        }
     }
 })
+
--- components/MediaPhotos.js
+++ components/MediaPhotos.js
@@ -2,16 +2,42 @@ const easy = Vue.createApp({
     data(){
         return {
            index: 1,
+           i_clicked: 1,
+           total_images: 15,
            fol_name: '',
-           score: 0
+           score: 0,
+           progress: 0,
+           finish_level: false,
+           badge_earned: false
         }
     },
+    computed:{
+        progressed(){
+            return this.progress.toFixed();
+        }
+    }, 
     methods:{
         log(message){
             console.log(message);
         },
-        increment_score(){
-            this.score = this.score + 1;
+        increment_score_win(index){
+            if( this.i_clicked == index){
+                this.score = this.score + 2;
+                this.increment_progress();
+                this.i_clicked += 1;
+            }
+        },
+        increment_score_lose(index){
+            if( this.i_clicked == index){
+                this.score = this.score + 1;
+                this.increment_progress();
+                this.i_clicked += 1;
+            }
+        },
+        increment_progress(){
+            if (this.progress < 100)
+                this.progress = Math.round((this.score / this.total_images /2) * 100);
+            
         },
         getImageMedia(index){
             in_string = index.toString();
@@ -26,12 +52,32 @@ const easy = Vue.createApp({
         next(){
             this.index = this.index + 1;
             this.log(this.index);
+            this.log(this.score);
 
-            m_name = this.getImageMedia(this.index);
-            document.getElementById('mediaId').src=m_name;
+            if(this.index <= this.total_images){
+                m_name = this.getImageMedia(this.index);
+                document.getElementById('mediaId').src=m_name;
 
-            r_name = this.getImageReal(this.index);
-            document.getElementById('realId').src=r_name;
+                r_name = this.getImageReal(this.index);
+                document.getElementById('realId').src=r_name;
+            }
+
+            else{
+                this.finish_level = true;
+                if(this.progress == 100){
+                    this.badge_earned = true;
+                    
+                    /*game.badges[0].earned = true;*/
+                    /*this.log(game.data.badges);*/
+                }
+            }
+        },
+        go_to_levels(){
+            window.location.href = "./instagram.html";
+        },
+        go_to_menu(){
+            window.location.href = "./menu.html";
         }
     }
 })
+
